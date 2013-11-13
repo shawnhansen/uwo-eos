1757d8ac291934234825c49a76260c20d0fe7d1e
Shawn Hansen <hansens@uwosh.edu>
2013-11-13 08:18:16 -0600

    Initial commit. Base yo angular install.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('eosApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
